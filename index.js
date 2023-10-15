9460820d354cbbee7bdb1472ebf1da04a3ecc692
pee47kay <papakowdadson@gmail.com>
2023-10-15 13:31:04 +0000

    done

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const blogRouter = require("./routes/blog/blogRouter");
-const dbRouter = require("./routes/blog/createDbRouter");
 const Dotenv = require("dotenv")
 
 const app = express();
@@ -14,7 +13,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.use('/blog',blogRouter);
-app.use('/createdb',dbRouter);
 
 app.listen(port,()=>{
     console.log('server running on port:',port)
